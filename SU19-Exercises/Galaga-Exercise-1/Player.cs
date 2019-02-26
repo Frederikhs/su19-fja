1e2c6d2c30a26e12b8c8b3ba15ededacf76692ce
andersfrimann <andersfrimann@hotmail.dk>
2019-02-26 16:58:21 +0100

    Working version

--- SU19-Exercises/Galaga-Exercise-1/Player.cs
+++ SU19-Exercises/Galaga-Exercise-1/Player.cs
@@ -1,24 +1,40 @@
-using System;
-using System.IO;
-using DIKUArcade;
-using DIKUArcade.EventBus;
-using DIKUArcade.Timers;
-using DIKUArcade.Graphics;
-using DIKUArcade.Physics;
 using DIKUArcade.Entities;
-using DIKUArcade.Input;
+using DIKUArcade.Graphics;
 using DIKUArcade.Math;
-using DIKUArcade.State;
-using DIKUArcade.Utilities;
 
 namespace Galaga_Exercise_1 {
     public class Player : Entity {
         private Game game;
+        private Shape shape;
+        
 
         public Player(Game game, DynamicShape shape, IBaseImage image)
             : base(shape, image) {
             this.game = game;
+            this.shape = shape;
+        }
+
+        public void Direction(Vec2F directionVector) {
+            var test = shape.AsDynamicShape();
+            test.ChangeDirection(directionVector);
+            shape = test;
 
         }
+
+        public void Move() {
+            
+            if (shape.Position.X >= 0.0 && shape.Position.X <= 0.9) {
+                shape.Move();    
+            } else if (shape.Position.X < 0.0) {
+                
+                shape.Position = new Vec2F(0.0f,0.0f);
+            } else {
+                shape.Position = new Vec2F(0.9f,0.0f);
+                
+            }
+            
+        }
     }
+    
+   
 }
