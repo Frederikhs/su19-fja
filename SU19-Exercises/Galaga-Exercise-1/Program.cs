6bbe72851f6e0c1809edfd30c16b0b20ba83cb66
andersfrimann <andersfrimann@hotmail.dk>
2019-02-26 12:52:24 +0100

    test

--- SU19-Exercises/Galaga-Exercise-1/Program.cs
+++ SU19-Exercises/Galaga-Exercise-1/Program.cs
@@ -4,6 +4,9 @@ namespace Galaga_Exercise_1 {
     internal class Program {
         public static void Main(string[] args) {
             Console.WriteLine("hej");
+            Console.WriteLine("Rend mig diku!");
+
+
         }
     }
 }
