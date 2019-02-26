4e54df4fe9a54418884155f94a2566b4537abda4
andersfrimann <andersfrimann@hotmail.dk>
2019-02-26 14:08:38 +0100

    Added game class

--- SU19-Exercises/Galaga-Exercise-1/Program.cs
+++ SU19-Exercises/Galaga-Exercise-1/Program.cs
@@ -3,10 +3,8 @@
 namespace Galaga_Exercise_1 {
     internal class Program {
         public static void Main(string[] args) {
-            Console.WriteLine("hej");
-            Console.WriteLine("Rend mig diku!");
-
-
+            Game NewGame = new Game();
+            NewGame.GameLoop();
         }
     }
 }
