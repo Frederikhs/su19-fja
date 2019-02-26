ac9fdb2b5af0846c98e5aabc73b9d7d8283a2a64
andersfrimann <andersfrimann@hotmail.dk>
2019-02-26 16:52:27 +0100

    merge

--- SU19-Exercises/Galaga-Exercise-1/Game.cs
+++ SU19-Exercises/Galaga-Exercise-1/Game.cs
@@ -1,23 +1,26 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using DIKUArcade;
 using DIKUArcade.EventBus;
 using DIKUArcade.Timers;
 using DIKUArcade.Graphics;
-using DIKUArcade.Physics;
 using DIKUArcade.Entities;
-using DIKUArcade.Input;
 using DIKUArcade.Math;
-using DIKUArcade.State;
-using DIKUArcade.Utilities;
 
 
 namespace Galaga_Exercise_1 {
 
+
+
+
     public class Game : IGameEventProcessor<object> {
         private Window win;
-        private DIKUArcade.Timers.GameTimer gameTimer;
         private Player player;
+        private DIKUArcade.Timers.GameTimer gameTimer;
+        private GameEventBus<object> eventBus;
+
+
 
         public Game() {
             // TODO: Choose some reasonable values for the window and timer constructor.
@@ -27,6 +30,17 @@ namespace Galaga_Exercise_1 {
             player = new Player(this,
                 new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
                 new Image(Path.Combine("Assets", "Images", "Player.png")));
+
+            eventBus = new GameEventBus<object>();
+            eventBus.InitializeEventBus(new List<GameEventType>() {
+                GameEventType.InputEvent, // key press / key release
+                GameEventType.WindowEvent, // messages to the window
+            });
+            win.RegisterEventBus(eventBus);
+            eventBus.Subscribe(GameEventType.InputEvent, this);
+            eventBus.Subscribe(GameEventType.WindowEvent, this);
+
+
         }
 
         public void GameLoop() {
@@ -36,11 +50,12 @@ namespace Galaga_Exercise_1 {
                     win.PollEvents();
 
                     // Update game logic here
+                    eventBus.ProcessEvents();
+                    player.Move();
                 }
 
                 if (gameTimer.ShouldRender()) {
                     win.Clear();
-                    // Render gameplay entities here
                     player.RenderEntity();
                     win.SwapBuffers();
                 }
@@ -50,20 +65,62 @@ namespace Galaga_Exercise_1 {
                     win.Title = "Galaga | UPS: " + gameTimer.CapturedUpdates +
                                 ", FPS: " + gameTimer.CapturedFrames;
                 }
+
             }
         }
 
-        public void KeyPress(string key) {
-            throw new NotImplementedException();
+
+        private void KeyPress(string key) {
+            switch (key) {
+            case "KEY_ESCAPE":
+                eventBus.RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.WindowEvent, this, "CLOSE_WINDOW", "", ""));
+                break;
+                ;
+            case "KEY_LEFT":
+                player.Direction(new Vec2F(-0.01f, 0.0f));
+                break;
+            case "KEY_RIGHT":
+                player.Direction(new Vec2F(0.01f, 0.0f));
+                break;
+            default:
+                break;
+
+            }
         }
 
         public void KeyRelease(string key) {
-            throw new NotImplementedException();
-        }
+                    //throw new NotImplementedException();
+                }
+
 
         public void ProcessEvent(GameEventType eventType,
             GameEvent<object> gameEvent) {
-            throw new NotImplementedException();
+            if (eventType == GameEventType.WindowEvent) {
+                switch (gameEvent.Message) {
+                case "CLOSE_WINDOW":
+                    win.CloseWindow();
+                    break;
+                default:
+                    break;
+                }
+            } else if (eventType == GameEventType.InputEvent) {
+                switch (gameEvent.Parameter1) {
+                case "KEY_PRESS":
+                    KeyPress(gameEvent.Message);
+                    break;
+                case "KEY_RELEASE":
+                    player.Direction(new Vec2F(0.0f, 0.0f));
+                    KeyRelease(gameEvent.Message);
+                    break;
+                default:
+                    break;
+                }
+            }
         }
-    }
-}
+
+            }
+        }
+    
+
