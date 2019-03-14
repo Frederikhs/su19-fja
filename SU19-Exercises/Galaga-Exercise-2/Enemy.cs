ea37d8f1c3003af3771ea206baa3c235a6aa868d
psl694 <jokke_2005@hotmail.com>
2019-03-14 16:52:45 +0100

    2.6 enemy class done

--- SU19-Exercises/Galaga-Exercise-2/Enemy.cs
+++ SU19-Exercises/Galaga-Exercise-2/Enemy.cs
@@ -1,16 +1,18 @@
+using System.Drawing.Text;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 
 namespace Galaga_Exercise_1 {
-    public class Enemy : Entity {
+    public class Enemy : DIKUArcade.Entities.Entity {
         private Game game;
         private Shape shape;
+        private Vec2F StartPos { get; }
 
         //SETTING SHAPE WHEN INITIATING CLASS
-        public Enemy(Game game, DynamicShape shape, IBaseImage image)
-            : base(shape, image) {
+        public Enemy(Game game, DynamicShape shape, IBaseImage image, Vec2F startPos) : base(shape, image) {
             this.game = game;
-            this.shape = shape;
+            StartPos = startPos;
         }
     }
 }
