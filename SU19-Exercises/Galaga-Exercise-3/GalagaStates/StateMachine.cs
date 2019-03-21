f40a638359ef684879b20763f5e17fb3fb412fb7
Frederik H.S <frederik@hoergreen.dk>
2019-03-21 11:52:58 +0100

    Code clean

--- SU19-Exercises/Galaga-Exercise-3/GalagaStates/StateMachine.cs
+++ SU19-Exercises/Galaga-Exercise-3/GalagaStates/StateMachine.cs
@@ -4,13 +4,13 @@ using Galaga_Exercise_3.GalagaStates;
 
 namespace GalagaGame.GalagaState {
     public class StateMachine : IGameEventProcessor<object> {
+        public IGameState ActiveState { get; private set; }
+
         public StateMachine() {
             GalagaBus.GetBus().Subscribe(GameEventType.GameStateEvent, this);
             GalagaBus.GetBus().Subscribe(GameEventType.InputEvent, this);
             ActiveState = MainMenu.GetInstance();
         }
-
-        public IGameState ActiveState { get; private set; }
 
         public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
             if (eventType == GameEventType.GameStateEvent) {
