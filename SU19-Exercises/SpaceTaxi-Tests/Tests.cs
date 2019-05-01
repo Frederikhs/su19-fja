d93f4678934dd6526deae9f9d5b4b782beb8e8d3
Frederik H.S <frederik@hoergreen.dk>
2019-05-01 09:36:45 +0200

    Added tests for the beach and added xml comments in tests.cs

--- SU19-Exercises/SpaceTaxi-Tests/Tests.cs
+++ SU19-Exercises/SpaceTaxi-Tests/Tests.cs
@@ -7,35 +7,25 @@ using SpaceTaxi_1;
 namespace SpaceTaxi_Tests {
     [TestFixture]
     public class Tests {
-        // Setting up a TextLoader with the short-n-sweet lvl and level part classes
-        private TextLoader textLoader;
-        private LvlCustomer lvlcustomer;
-        private LvlInfo lvlinfo;
-        private LvlLegends lvllegends;
-        private LvlStructures lvlstructures;
-        [SetUp] 
-        public void InitiateTextLoader() {
-            textLoader = new TextLoader("short-n-sweet");
-            lvlcustomer = new LvlCustomer("short-n-sweet");
-            lvlinfo = new LvlInfo("short-n-sweet");
-            lvllegends = new LvlLegends("short-n-sweet");
-            lvlstructures = new LvlStructures("short-n-sweet");
-        }
         
-        //Testing that our textloader and lvlcustomer class finds the correct customer
-        //info line in the level short-n-sweet text file.
+        /// <summary>
+        /// Testing that we get the correct customer(s) & structure for short-n-sweet
+        /// </summary>
         [Test]
-        public void LvlCustomer() {
+        public void short_n_sweet_LvlCustomer() {
+            LvlCustomer lvlcustomer = new LvlCustomer("short-n-sweet");
             List<String> Customers = new List<string> {
                 "Customer: Alice 10 1 ^J 10 100"
             };
             Assert.AreEqual(Customers,lvlcustomer.Customers);
         }
 
-        //Testing that our textloader and lvlinfo class defines the same dictionary
-        //key value store for the level short-n-sweet text file
+        /// <summary>
+        /// Testing that we get the correct lvl info & structure for short-n-sweet
+        /// </summary>
         [Test]
-        public void LvlInfo() {
+        public void short_n_sweet_LvlInfo() {
+            LvlInfo lvlinfo = new LvlInfo("short-n-sweet");
             Dictionary<string, string> infoDic = new Dictionary<string, string> {
                 {"Name","SHORT -N- SWEET"},
                 {"Platforms","1"}
@@ -43,10 +33,12 @@ namespace SpaceTaxi_Tests {
             CollectionAssert.AreEqual(infoDic,lvlinfo.InfoDic);
         }
 
-        //Testing that our textloader and lvllegends class defines the same dictionary
-        //key value store for the level short-n-sweet text file
+        /// <summary>
+        /// Testing that we get the correct lvl legends & structure for short-n-sweet
+        /// </summary>
         [Test]
-        public void LvlLegends() {
+        public void short_n_sweet_LvlLegends() {
+            LvlLegends lvllegends = new LvlLegends("short-n-sweet");
             Dictionary<char, string> LegendsDic = new Dictionary<char, string> {
                 {'%', "white-square.png"},
                 {'#', "ironstone-square.png"},
@@ -74,10 +66,12 @@ namespace SpaceTaxi_Tests {
             CollectionAssert.AreEqual(LegendsDic,lvllegends.LegendsDic);
         }
         
-        //Testing that our textloader and lvlstructure class creates the
-        //same string list for the level short-n-sweet in the text file.
+        /// <summary>
+        /// Testing that we get the correct lvl structure for short-n-sweet
+        /// </summary>
         [Test]
-        public void LvlStructure() {
+        public void short_n_sweet_LvlStructure() {
+            LvlStructures lvlstructures = new LvlStructures("short-n-sweet");
             List<String> Structure = new List<string> {
                 "%#%#%#%#%#%#%#%#%#^^^^^^%#%#%#%#%#%#%#%#",
                 "#               JW      JW             %",
@@ -105,5 +99,106 @@ namespace SpaceTaxi_Tests {
             };
             Assert.AreEqual(Structure,lvlstructures.Structure);
         }
+        
+        /// <summary>
+        /// Testing that we get the correct customer(s) & structure for the-beach
+        /// </summary>
+        [Test]
+        public void the_beach_LvlCustomer() {
+            LvlCustomer lvlcustomer = new LvlCustomer("the-beach");
+            List<String> Customers = new List<string> {
+                "Customer: Bob 10 J r 10 100",
+                "Customer: Carol 30 r ^ 10 100"
+            };
+            Assert.AreEqual(Customers,lvlcustomer.Customers);
+        }
+        
+        /// <summary>
+        /// Testing that we get the correct lvl info & structure for the-beach
+        /// </summary>
+        [Test]
+        public void the_beach_LvlInfo() {
+            LvlInfo lvlinfo = new LvlInfo("the-beach");
+            Dictionary<string, string> infoDic = new Dictionary<string, string> {
+                {"Name","THE BEACH"},
+                {"Platforms","J, i, r"}
+            };
+            CollectionAssert.AreEqual(infoDic,lvlinfo.InfoDic);
+        }
+        
+        /// <summary>
+        /// Testing that we get the correct lvl legends & structure for the-beach
+        /// </summary>
+        [Test]
+        public void the_beach_LvlLegends() {
+            LvlLegends lvllegends = new LvlLegends("the-beach");
+            Dictionary<char, string> LegendsDic = new Dictionary<char, string> {
+                {'A', "aspargus-edge-left.png"},
+                {'B', "aspargus-edge-right.png"},
+                {'T', "aspargus-edge-top.png"},
+                {'C', "aspargus-edge-top-left.png"},
+                {'D', "aspargus-edge-top-right.png"},
+                {'G', "white-left-half-circle.png"},
+                {'H', "white-right-half-circle.png"},
+                {'I', "white-square.png"},
+                {'J', "white-square.png"},
+                {'O', "olive-green-square.png"},
+                {'S', "salt-box-square.png"},
+                {'M', "minsk-square.png"},
+                {'a', "emperor-square.png"},
+                {'b', "emperor-lower-right.png"},
+                {'c', "emperor-lower-left.png"},
+                {'d', "emperor-upper-left.png"},
+                {'e', "deep-bronze-square.png"},
+                {'f', "deep-bronze-left-half-circle.png"},
+                {'g', "deep-bronze-right-half-circle.png"},
+                {'i', "ironstone-square.png"},
+                {'j', "ironstone-square.png"},
+                {'l', "ironstone-lower-left.png"},
+                {'u', "ironstone-upper-right.png"},
+                {'o', "studio-square.png"},
+                {'p', "studio-upper-right.png"},
+                {'q', "studio-lower-left.png"},
+                {'r', "studio-square.png"},
+                {'t', "tacha-square.png"},
+                {'s', "tacha-upper-right.png"}
+            };
+            CollectionAssert.AreEqual(LegendsDic,lvllegends.LegendsDic);
+        }
+        
+        /// <summary>
+        /// Testing that we get the correct lvl structure for the-beach
+        /// </summary>
+        [Test]
+        public void the_beach_LvlStructure() {
+            LvlStructures lvlstructures = new LvlStructures("the-beach");
+            List<String> Structure = new List<string> {
+                "CTTTTTTTTTTTTTTTTD^^^^^^CTTTTTTTTTTTTttt",
+                "A                                    stt",
+                "A                                      B",
+                "A HJJJJJJJJG                           B",
+                "HIIIIIIIIIG                            B",
+                "A  HIIIG                               B",
+                "A                     prrrrrq          B",
+                "A                      poooooqbc       B",
+                "A                       poooooad       B",
+                "A                        poooooq       B",
+                "A                         poooooq      B",
+                "A                          pooooo      B",
+                "A                           aoooo      B",
+                "A                           apooo      B",
+                "A              >            a poo      B",
+                "A                           a  po      B",
+                "A ujl                       a   p      B",
+                "A  ujl                      a          B",
+                "A   ujl                     a          B",
+                "A    ujiiiiiiiiiiiiiij      a          B",
+                "A     gef         gef       a          B",
+                "MMMMMMMeMMMMMMMMMMMeMMMMMMMMaMMMMMMMMMMM",
+                "OOOOSSSeSSSSSSSSSSSeSSSSSSSOaOOOOOOOOOOO"
+            };
+            Assert.AreEqual(Structure,lvlstructures.Structure);
+        }
+        
     }
 }
